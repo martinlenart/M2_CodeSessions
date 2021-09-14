55cd0f0610fc48139576dcf70cabbd0d33db8d22
Martin Lenart <martin_lenart@icloud.com>
2021-09-14 12:11:50 +0200

    Added NestedLoops project

---
+++ NestedLoops/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace NestedLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Nested while loops
+            Console.WriteLine("Nested while-loops");
+            int i = 0;
+            while (i < 10)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"i: {i++}");
+
+                int k = 0;
+                while (k < 10)
+                {
+                    Console.WriteLine($"k: {k++}");
+                }
+            }
+
+
+            //Nested for-loops
+            Console.WriteLine();
+            Console.WriteLine("Nested for-loops");
+            for (int j = 10; j >= 0; j--)
+            {
+                Console.WriteLine($"i: {j}");
+                for (int k = 1; k <= 10; k++)
+                {
+                    Console.WriteLine($"k: {k}");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
