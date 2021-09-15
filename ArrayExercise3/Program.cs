7073e0a061aa37eaccebd33f321f01a629e5443c
Martin Lenart <martin_lenart@icloud.com>
2021-09-15 16:51:57 +0200

    ArrayExercise3 update

---
+++ ArrayExercise3/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace ArrayExercise3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] stringArray = { "Hello", "my", "name", "is", "Martin" };
+
+            string search = "Martin";
+            bool found;
+
+            FindString(stringArray, search, out found);
+
+            if (found)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"{search} found");
+            }
+
+
+            Console.WriteLine();
+        }
+
+        private static void FindString(string[] stringArray, string search, out bool found)
+        {
+            found = false;
+            foreach (var item in stringArray)
+            {
+                if (item == search)
+                {
+                    found = true;
+                }
+
+                if (item == "name" || item == "is")
+                    continue;
+
+                Console.Write($"{item} ");
+            }
+
+            Console.WriteLine();
+            for (int i = 0; i < stringArray.Length; i++)
+            {
+                if (stringArray[i] == "name" || stringArray[i] == "is")
+                    continue;
+
+                Console.Write($"{stringArray[i]} ");
+
+            }
+        }
+    }
+}
--- CodeStructure/Program.cs
+++ CodeStructure/Program.cs
@@ -2,10 +2,35 @@
 
 namespace CodeStructure
 {
+    //Here can I ONLY declare types
+    //For example, enum ??, struct ??, class ?? record ??
+    public struct Example1
+    {
+        int X, Y;
+    }
+
     class Program
     {
+        //Here I can declare types, members (data and function)
+        public struct Example2
+        {
+            int X, Y;
+        }
+        int Member1 = 0;
+        int Member2 = 0;
+
+        bool IsEqual (int param1, int param2)
+        {
+            //Here I can declare variables and write code
+
+            return param1 == param2;
+        }
+
+
         static void Main(string[] args)
         {
+            //Here I can declare variables and write code
+            
             Console.WriteLine("Hello World!");
         }
     }
