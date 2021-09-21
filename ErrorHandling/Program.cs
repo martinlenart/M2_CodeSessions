2f0c78548bbc12d916dc23ccc1a6aa4bbfc2a039
Martin Lenart <martin_lenart@icloud.com>
2021-09-21 16:14:27 +0200

    ErrorHandling added

---
+++ ErrorHandling/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace ErrorHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                ProcessUserInput();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Unfortunatly, without memory I cannot continue {ex.Message}");
+            }
+        }
+
+        private static void ProcessUserInput()
+        {
+            bool quit = false;
+            Console.WriteLine("Don't Press Button 5 or 7!");
+            do
+            {
+                Console.WriteLine("Which button do you want to press?");
+                string sButtonToPress = Console.ReadLine();
+
+                if (sButtonToPress == "q")
+                {
+                    quit = true;
+                }
+                else
+                {
+                    int buttonToPress;
+                    if (int.TryParse(sButtonToPress, out buttonToPress))
+                    {
+                        try
+                        {
+                            PressTheButton(buttonToPress);
+                            Console.WriteLine("Indeed the button was pressed successfully");
+                        }
+                        catch (InsufficientMemoryException ex)
+                        {
+                            Console.WriteLine("Why did you press button 7! We ran out of memory");
+                            throw;
+                            //throw new Exception("Sorry, you are hopeless");
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine($"I told you not to press button 5!! {ex.Message}");
+                        }
+                        finally
+                        {
+                            Console.WriteLine("Yes, the button was pressed");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Wrong input, try again");
+                    }
+                }
+            } while (!quit);
+        }
+
+        static void PressTheButton(int buttonNr)
+        {
+            if (buttonNr == 5)
+                throw new Exception("KaBoom!!");
+
+            if (buttonNr == 7)
+                throw new InsufficientMemoryException("You hopless guy!");
+
+            Console.WriteLine($"You pressed button {buttonNr}");
+        }
+    }
+}
