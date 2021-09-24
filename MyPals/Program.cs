69f0f34ec171a5979c4acc689d3d491919a14ac4
Martin Lenart <martin_lenart@icloud.com>
2021-09-24 14:06:36 +0200

    MyPals and Statics added

---
+++ MyPals/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace MyPals
+{
+    public enum FriendLevel
+    {
+        Casual, Good, Super, Family
+    }
+    public struct Friend
+    {
+        public string FirstName;       //Both FirstName and LastName equals null means an empty space in myFriends
+        public string LastName;
+        public FriendLevel Level;
+        public string PhoneNr;
+        public DateTime BirthDate;
+    }
+
+    class Program
+    {
+        public const int MaxNrOfFriends = 30;
+        static void Main(string[] args)
+        {
+            Friend[] myFriends = new Friend[MaxNrOfFriends];
+
+            Console.WriteLine($"My list of friends has place for {MaxNrOfFriends} friends");
+            Console.WriteLine($"I have right now {NrOfFriends(myFriends)} friends  :-(");
+
+            myFriends[0] = new Friend { FirstName = "John", LastName = "Smith", BirthDate = DateTime.Parse("1964-03-25"), Level = FriendLevel.Super, PhoneNr = "+1 (312) 556 234" };
+            Console.WriteLine($"I have right now {NrOfFriends(myFriends)} friend :-)");
+
+        }
+
+        public static int NrOfFriends (Friend[] Friends)
+        {
+            int nrOfFriends = 0;
+            foreach (var item in Friends)
+            {
+                if (item.FirstName != null && item.LastName != null)
+                    nrOfFriends++;
+            }
+
+            return nrOfFriends;
+        }
+    }
+}
---
+++ StaticVsNonStatic/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace StaticVsNonStatic
+{
+    public class Friend
+    {
+        public string Name;
+        public int Age;
+
+        public static int NrOfFriends = 0;
+        public static int Oldest = int.MinValue;
+
+        public Friend (string name, int age)
+        {
+            Name = name;
+            Age = age;
+
+            Console.WriteLine($"Executing contructor for {Name}");
+            NrOfFriends++;
+
+            if (Age > Oldest)
+            {
+                Oldest = Age;
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {                       
+            Friend friend1 = new Friend ("John", 30);
+            Friend friend2 = new Friend ("Mary", 40);
+
+            Console.WriteLine($"I have {Friend.NrOfFriends} friends");
+            Console.WriteLine($"My oldest friend is {Friend.Oldest} years old.");
+
+        }
+    }
+}
